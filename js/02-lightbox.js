1f19b9a02323782d427bf34688ef11afe30152fa
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-04 20:13:27 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,26 +3,28 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-
 const galleryEl = document.querySelector('.gallery');
 const imgMarkup = createsMarkupGalleryItems(galleryItems);
 
 galleryEl.insertAdjacentHTML('beforeend', imgMarkup);
 
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 function createsMarkupGalleryItems(galleryItems) {
   return galleryItems
-    .map((items) => {
-      return `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    .map(items => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${items.original}">
     <img
         class="gallery__image"
         src="${items.preview}"
         alt="${items.description}"
     />
     </a>
-</div>`;
+</li>`;
     })
     .join('');
 }
-
-console.log(imgMarkup);
